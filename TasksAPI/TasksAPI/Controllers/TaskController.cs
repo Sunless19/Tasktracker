2d8787c9120b50788ab9645c956518b3acf28c36
Sunless19 <onofreisergiu70@gmail.com>
2024-05-28 18:36:44 +0300

    Uploaded content

---
+++ TasksAPI/TasksAPI/Controllers/TaskController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TasksAPI.Models;
+using TasksAPI.Services;
+
+[ApiController]
+[Route("[controller]")]
+public class TaskController : ControllerBase
+{
+
+
+    private readonly ITaskCollectionService _taskCollectionService;
+
+    public TaskController(ITaskCollectionService taskCollectionService)
+    {
+        _taskCollectionService = taskCollectionService ?? throw new ArgumentNullException(nameof(TaskCollectionService));
+    }
+
+    
+
+    [HttpPost]
+    public async Task<IActionResult> CreateTask([FromBody] TaskModel taskModel)
+    {
+        if (taskModel == null)
+        {
+            return BadRequest("Task cannot be null");
+        }
+
+        // to implement create task 
+        await _taskCollectionService.Create(taskModel);
+        return Ok(taskModel.Id);
+    }
+   
+
+    [HttpGet]
+    public async Task<IActionResult> GetTasks()
+    {
+        List<TaskModel> tasks = await _taskCollectionService.GetAll();
+        return Ok(tasks);
+    }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteTask(Guid id)
+    {
+        try
+        {
+            var taskToDelete = await _taskCollectionService.GetAll();
+            if (taskToDelete == null)
+            {
+                return NotFound("Task not found");
+            }
+
+            await _taskCollectionService.Delete(id);
+            return Ok("Task deleted successfully");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, "Error in processing the task");
+        }
+    }
+
+
+    //[HttpGet("Tasks")]
+    //public IActionResult GetTasks()
+    //{
+    //    return Ok(_tasks);
+    //}
+
+    //[HttpPost("Task")]
+    //public IActionResult CreateTask([FromBody] TaskModel task)
+    //{
+    //    if (task == null)
+    //    {
+    //        return BadRequest("Task cannot be null");
+
+    //    }
+
+    //    task.Id = Guid.NewGuid();
+    //    _tasks.Add(task);
+    //    return Ok(_tasks);
+    //}
+
+    //[httpput("updatetask")]
+    //public iactionresult updatetask(guid id, [frombody] taskmodel updatedtask)
+    //{
+    //    if(updatedtask==null)
+    //    { return badrequest("task was empty"); }
+
+    //    taskmodel existingtask = _tasks.firstordefault(t => t.id == id);
+
+    //    if(updatedtask.id!=existingtask.id)
+    //    {
+    //        return badrequest("task id in the body must match the route parameter id");
+    //    }
+
+    //    if (existingtask == null) { return notfound(id); }
+
+    //    existingtask.title= updatedtask.title;
+    //    existingtask.description=updatedtask.description;
+    //    existingtask.assignedto = updatedtask.assignedto;
+    //    existingtask.status = updatedtask.status;
+
+    //    return ok(existingtask);
+    //}
+
+    //[HttpDelete("Task/{id}")]
+    //public IActionResult DeleteTask(Guid id)
+    //{
+    //    try
+    //    {
+    //        TaskModel taskToDelete = _tasks.FirstOrDefault(t => t.Id == id);
+
+    //        if (taskToDelete == null)
+    //        {
+    //            return NotFound("Task not found");
+    //        }
+
+    //        _tasks.Remove(taskToDelete);
+    //        return Ok("Task deleted successfully");
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        return StatusCode(StatusCodes.Status500InternalServerError, "Error in processing the Task");
+    //    }
+    //}
+}
---
+++ TasksAPI/TasksAPI/Services/TaskCollectionService.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using TasksAPI.Models;
+using TasksAPI.Settings;
+
+namespace TasksAPI.Services
+{
+    public class TaskCollectionService : ITaskCollectionService
+    {
+        private readonly IMongoCollection<TaskModel> _tasks;
+
+        public TaskCollectionService(IMongoDBSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _tasks = database.GetCollection<TaskModel>(settings.TasksCollectionName);
+        }
+
+        public async Task<List<TaskModel>> GetAll()
+        {
+            var result = await _tasks.FindAsync(task => true);
+            return result.ToList();
+        }
+        public async Task<bool> Create(TaskModel taskModel)
+        {
+            if (taskModel.Id == Guid.Empty) {
+                taskModel.Id = Guid.NewGuid();
+            }
+
+            await _tasks.InsertOneAsync(taskModel);
+            return true;
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            var result = await _tasks.DeleteOneAsync(taskModel => taskModel.Id == id);
+            if (!result.IsAcknowledged && result.DeletedCount == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+       
+
+        public async Task<TaskModel> Get(Guid id)
+        {
+            return (await _tasks.FindAsync(taskModel => taskModel.Id == id)).FirstOrDefault();
+        }
+
+        public async Task<bool> Update(Guid id, TaskModel taskModel)
+        {
+            taskModel.Id = id;
+            var result = await _tasks.ReplaceOneAsync(taskModel => taskModel.Id == id, taskModel);
+            if (!result.IsAcknowledged && result.ModifiedCount == 0) {
+                await _tasks.InsertOneAsync(taskModel);
+                return false;
+            }
+            
+            return true;
+        }
+
+        public async Task<List<TaskModel>> GetTasksByStatus(string status)
+        {
+            return (await _tasks.FindAsync(taskModel => taskModel.Status == status)).ToList();
+        }
+
+
+        //List<TaskModel> _tasks = new List<TaskModel> { new TaskModel { Id = Guid.NewGuid(), Title = "First Task", Description = "First Task Description" , AssignedTo = "Author_1", Status = "To do"},
+        //new TaskModel { Id = Guid.NewGuid(), Title = "Second Task", Description = "Second Task Description", AssignedTo = "Author_1", Status = "To do" },
+        //new TaskModel { Id = Guid.NewGuid(), Title = "Third Task", Description = "Third Task Description", AssignedTo = "Author_2", Status = "To do"  },
+        //new TaskModel { Id = Guid.NewGuid(), Title = "Fourth Task", Description = "Fourth Task Description", AssignedTo = "Author_3", Status = "To do"  },
+        //new TaskModel { Id = Guid.NewGuid(), Title = "Fifth Task", Description = "Fifth Task Description", AssignedTo = "Author_4", Status = "To do"  }
+        //};
+        //public List<TaskModel> GetAll()
+        //{
+        //    return _tasks;
+        //}
+
+        //public bool Create(TaskModel model)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public bool Delete(Guid id)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //[HttpGet]
+        //public List<TaskModel> GetTasks()
+        //{
+        //    return _tasks;
+        //}
+
+        //public TaskModel Get(Guid id)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+
+
+        //public List<TaskModel> GetTasksByStatus(string status)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public bool Update(Guid id, TaskModel model)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
