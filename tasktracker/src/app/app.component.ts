4062a0baf8d126b79f15b5d767b1d96fed545878
Sunless19 <onofreisergiu70@gmail.com>
2024-05-29 13:35:56 +0300

    Frontend aesthetics

--- tasktracker/src/app/app.component.ts
+++ tasktracker/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { TasksViewComponent } from './tasks-view/tasks-view.component';
 import { RouterModule } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, TasksViewComponent, RouterModule],
+  imports: [RouterOutlet, MatToolbarModule, TasksViewComponent, RouterModule,MatIcon],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
